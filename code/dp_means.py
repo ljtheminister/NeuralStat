5059d43c78efceb641470485b287527fbc3f0040
ljtheminister <john.c.min@gmail.com>
2013-12-07 16:20:20 -0500

    updating still

--- code/dp_means.py
+++ code/dp_means.py
@@ -11,7 +11,28 @@ class DP_means(self):
         self.clusters = dp_means(freq,lamb)
 
     def __dp_means(self, freq, lamb):
+	## INITIALIZATION
         clusters = Clusters()
         clusters.c = 1
         m1 = sum([x*freq[x] for x in freq.keys()])/float(len(freq.keys()))  
         clusters.assignments[
+	clusters.map = []
+
+
+    def __check_convergence(self, old_clusters, new_clusters, epsilon):
+
+        if old_clusters.c != new_clusters.c:
+            return False
+
+        else:
+            diff = np.subtract(new_clusters.assignments.keys(), old_clusters.assignments.keys())
+            for elem in diff:
+                if elem > epsilon: 
+                    return False
+            return True
+
+
+
+
+
+
